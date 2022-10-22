7be18f22037a9809b0e7918496b7194b95744562
Paulo <pmarquesbaronefilho15@gmail.com>
2022-10-21 23:43:16 -0300

    validation tip input finished

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,46 @@
 const tips = document.querySelectorAll('.tips');
+const billInput = document.querySelector('#bill');
 const tipCustom = document.querySelector('.tip-custom')
 const numberPeople = document.querySelector('.people-number-input');
 
-tips.forEach((val) => {
-  val.addEventListener('click', checkClick)
-})
-if (tipCustom.value) tipCustom.classList.add('input-selected');
+const tipsContainer = document.querySelector('.tips-container');
 
-function checkClick(e) {
+const billErrorSpan = document.querySelector('.bill-error');
+const tipErrorSpan = document.querySelector('.tip-error');
+const peopleErrorSpan = document.querySelector('.people-error');
+
+applyError(billErrorSpan, 'The field cannot be empty');
+applyError(tipErrorSpan, 'You must select an option');
+applyError(peopleErrorSpan, 'The field cannot be empty');
+
+tips.forEach((val) => {
+  val.addEventListener('click', checkTip)
+})
+
+function checkTip(e) {
   tips.forEach((val) => {
-    val.classList.remove('selected')
+    val.classList.remove('selected');
     tipCustom.classList.remove('input-selected')
     tipCustom.value = null
-    if(e.target.innerHTML == val.innerHTML) val.classList.add('selected');
+    if(e.target.innerHTML == val.innerHTML) {
+      val.classList.add('selected')
+      if(tipsContainer.querySelector('.selected') || tipsContainer.querySelector('.input-selected')) removeError(tipErrorSpan)
+    }
   })
 }
 
-tipCustom.addEventListener('change', () => {
-  if(tipCustom.value.length >= 0 && tipCustom.value.slice(-1) != '%') tipCustom.value += '%';
+tipCustom.addEventListener('blur', () => {
+  if (tipCustom.value.length >= 1 && tipCustom.value.slice(-1) != '%') tipCustom.value += '%';
   if (tipCustom.value.length >= 1) tipCustom.classList.add('input-selected');
+  if(!tipsContainer.querySelector('.selected') && !tipsContainer.querySelector('.input-selected')) applyError(tipErrorSpan, 'You must select an option');
 })
 
-numberPeople.addEventListener('change', () => {
-  if(numberPeople.value < 1) numberPeople.customValidity()
-})
+function applyError(tag, msg) {
+  tag.style.display = "block"
+  tag.innerHTML = msg
+}
+
+function removeError(tag) {
+  tag.style.display = "none"
+  tag.innerHTML = ''
+}
